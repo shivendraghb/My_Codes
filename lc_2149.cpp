dfc317453b46b78aa5ffd796c93f76d9e5abfd1f
Shivendra Pratap Srivastava <shivendrapratapsrivastava69@gmail.com>
2025-10-18 19:59:05 +0530

    lc_2149

---
+++ lc_2149.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
+
+    vector<int> ans(n);
+    int pos = 0, neg = 1;
+    for(int x : v) {
+        if(x > 0) {
+            ans[pos] = x;
+            pos += 2;
+        } else {
+            ans[neg] = x;
+            neg += 2;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        cout << ans[i] << ' ';
+    }
+    cout << '\n';
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
