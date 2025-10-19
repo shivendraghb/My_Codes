4b99bbf04de6f9faa22545863db7ef55dcb811f2
Shivendra Pratap Srivastava <shivendrapratapsrivastava69@gmail.com>
2025-10-19 23:08:36 +0530

    lc_3081

---
+++ lc_3081.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+    string s;
+    cin >> s;
+
+    vector<int> freq(26, 0);
+    string temp = "";
+
+    for(auto c : s) if(c != '?') freq[c - 'a']++;
+    
+    for(auto ind = 0; ind < s.size(); ind++) {
+        if(s[ind] != '?') continue;
+        int minFreqInd = 0, minFreq = INT_MAX;
+        for(int i = 0; i < 26; i++) {
+            if(minFreq > freq[i]) {
+                minFreq = freq[i];
+                minFreqInd = i;
+            }
+        }
+        temp += ('a' + minFreqInd);
+        freq[minFreqInd]++;
+    }
+
+    sort(temp.begin(), temp.end());
+    for(int i = 0, j = 0; i < s.size(); i++)
+        if(s[i] == '?') s[i] = temp[j++];
+
+    cout << s << '\n';
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
